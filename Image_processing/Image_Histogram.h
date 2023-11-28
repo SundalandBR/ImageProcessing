eeaed01a8d1e0f75f01062e032ff20c9917492d0
Sundaland <b449024525@outlook.com>
2023-11-28 15:27:03 +0800

    e

---
+++ Image_processing/Image_Histogram.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Image_opencv.h"
+
+static struct _cdf_
+{
+	int array[256];
+	int MAX_CDF;
+	int	MIN_CDF;
+};
+
+
+void Imgae_Histogram(cv::InputArray src, cv::OutputArray dst);
+static void cdf(cv::InputArray src, struct _cdf_* _CDF);
